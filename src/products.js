25cbb0135b236ad5b3c49535febcdf3965de95e6
Leonela <maria.quispe.27@unsch,edu.pe>
2026-05-11 01:04:56 -0500

    modificación products

--- src/products.js
+++ src/products.js
@@ -1,6 +1,6 @@
 // products.js — Módulo de gestión de productos
 // LABORATORIO 02: Este código tiene defectos intencionales
-var products = [
+const products = [
 { id: 1, name: 'Laptop HP', price: 2500},
 { id: 2, name: 'Mouse Logitech', price: null},
 { id: 3, name: 'Teclado Mecánico', price: -50 },
@@ -8,8 +8,8 @@ var products = [
 ]
 // Busca un producto por ID
 function getProductById(id) {
-var result = null;
-for (var i = 0; i < products.length; i++) {
+let result = null;
+for (let i = 0; i < products.length; i++) {
 console.log("Iteración:", i);
 if (products[i].id ==id) {
 result = products[i];
@@ -21,7 +21,7 @@ const product = getProductById(2);
 console.log(product, 'individual')
 // Aplica descuento al precio del producto
 function calculateDiscount(product, discount) {
-var finalPrice = product.price - (product.price * discount);
+const finalPrice = product.price - (product.price * discount);
 return finalPrice; // ← ¿qué pasa si price es null?
 }
 const discounted = calculateDiscount(product, 0.1);
